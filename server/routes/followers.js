4da6be638a65c442bedeb6f7538ba3e41d405b7c
Oleg Zaichkin <trueolegzaichkin@gmail.com>
2018-07-15 08:35:48 +0300

    subscribe

--- server/routes/followers.js
+++ server/routes/followers.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const sequelize = require('../models/sequelize');
+const models = require('../models/sequelize');
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 const auth = require('../helpers/auth');
@@ -11,12 +12,18 @@ router.post('/', verify, async (req, res, next) => {
         // const data = req.body;
         const followingId = req.query.id;
         const userId = req._userId;
-        await sequelize.query(`INSERT INTO followers (follower, following) VALUES (:follower, :following)`,
-        {replacements: {follower: `${userId}`, following: `${followingId}`}, type: sequelize.QueryTypes.INSERT})
+        const follower = await models.Followers.build({ follower: userId, following: followingId})
+                                                .save();
         res.status(200).send({
             message: 'success',
             result: true,
-        }) 
+        })                                         
+        // await sequelize.query(`INSERT INTO followers (follower, following) VALUES (:follower, :following)`,
+        // {replacements: {follower: `${userId}`, following: `${followingId}`}, type: sequelize.QueryTypes.INSERT})
+        // res.status(200).send({
+        //     message: 'success',
+        //     result: true,
+        // }) 
     }
     catch(err) {
         next(new Error(err.message));
@@ -28,12 +35,17 @@ router.delete('/', verify, async (req, res, next) => {
         // const data = req.body;
         const followingId = req.query.id;
         const userId = req._userId;
-        await sequelize.query(`DELETE FROM followers WHERE following=:following AND follower=:follower`, 
-        {replacements: {following: `${followingId}`, follower: `${userId}`}, type: sequelize.QueryTypes.DELETE})
+        const follower = await models.Followers.destroy({where: {follower: userId, following: followingId}})
         await res.status(200).send({
-              message: 'success',
+              message: 'delete',
               result: true,
         })
+        // await sequelize.query(`DELETE FROM followers WHERE following=:following AND follower=:follower`, 
+        // {replacements: {following: `${followingId}`, follower: `${userId}`}, type: sequelize.QueryTypes.DELETE})
+        // await res.status(200).send({
+        //       message: 'success',
+        //       result: true,
+        // })
     }
     catch(err) {
         next(new Error(err.message));
